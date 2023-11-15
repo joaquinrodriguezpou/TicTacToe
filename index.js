1f061f9fe1fc86822e1160704a5118c6f6f1726c
Joaquin <138231434+joaquinrodriguezpou@users.noreply.github.com>
2023-11-15 18:09:32 -0300

    define game variables

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const buttons = document.getElementById('buttons');
 const winnerContainer = document.getElementById('winner-container');
 const winnerScreen = document.getElementById('winner');
 const restartBtn = document.getElementById('Restart');
+
+let user;
+let computer;
+let winner;
+let board;
+let recomendedGameComp;
+let recomendedGameUser;
